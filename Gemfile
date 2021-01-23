4f4f39238119241014c693521282cf69d34c37d5
t-tech <t.techa2020@gmail.com>
2021-01-23 14:39:27 +0000

    cloudinary

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'kaminari'
 
 gem 'carrierwave'
+gem 'cloudinary'
 
 gem 'rails-i18n'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :destroy, :followings, :followers, :likes]
+  before_action :correct_user, only: [:destroy]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
