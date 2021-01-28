a966e33bb51feefb19147dfa496d3c3b88814f24
t-tech <t.techa2020@gmail.com>
2021-01-28 14:06:35 +0000

    fix 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :destroy, :followings, :followers, :likes]
-  before_action :correct_user, only: [:destroy]
+  before_action :current_user, only: [:destroy]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
